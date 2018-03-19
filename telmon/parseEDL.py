47b9b30144db6d3e96da9f71f73e9efbcfa9152f
srlewein <srlewein@usgs.gov>
2018-03-19 07:22:36 -0500

    Completed file parse

--- telmon/parseEDL.py
+++ telmon/parseEDL.py
@@ -1,5 +1,6 @@
 import re
 import io
+from time import gmtime, strftime, time
 
 def parseFile(filepath):
     """
@@ -52,7 +53,7 @@ def parseStream(input):
     return parsedLines
     
 	
-class _EdlLine:
+class EdlFile:
     """
         Parse a line using regex to determine line type
         Throws RuntimeException if passed a null value
@@ -60,43 +61,78 @@ class _EdlLine:
         
         Values are returned as simple lists, caller will need to know 
         what to do with lists.
+        
+        JSON structure of an EDL file
+        {
+            "filename": "afile name",
+            "filedtg": "2017-3-2017 19:00:00",
+            "SOURCE": "nwisa",
+            "STATION": "12113390",
+            "DEVICE": "GRFILTF-1",
+            "OVERWRITE": "true",
+            "timeanddatum": [ 
+                ["2018-02-19 02:00", "3188.3097222222264"],
+                ["2018-02-19 03:00", "3188.3097222222264"]
+                ]
+        }
+            
     """
-	
+   
+   
+    filename = None
+    filedtg = None
+    SOURCE = None
+    STATION = None
+    DEVICE = None
+    OVERWRITE = None
+    timeanddatum = []
 
-    retVal = None
-	
-    def __init__(self, line):
+    def __init__(self, fileName, filedate):
+        #If No name is assigned, make one up
+        if fileName:
+             self.filename = fileName
+        else:
+             suffix = str(int(round(time())))
+             self.filename = 'assigned' + suffix
+        if filedate:
+            if re.search('\d{4}\-\d{1,2}\-\d{1,2}\s\d{2}\:\d{2}\:\d{2}',filedate):
+                self.filedtg = filedate
+            else:
+                err = 'Date format should be 2017-3-2017 19:00:00 NOT: ' + filedate
+                raise ValueError(err) 
+        else:
+            self.filedtg = strftime("%Y-%m-%d %H:%M:%S", gmtime())     
+            
+    def parseline(self, line):
 	# check on what kind of a line has been received, then parse it
         if(line == None):
             raise RuntimeError('Null Value') 
         if re.search('SOURCE*',line):
             a = line.split(' ');
             source = a[1]
-            self.retVal = ['SOURCE', source]
+            self.SOURCE = source
             return 
         if re.search('STATION*',line):
             a = line.split(' ');
             station = a[1]
-            self.retVal = ['STATION', station]
+            self.STATION = station
             return 
         if re.search('DEVICE*',line):
             a = line.split(' ');
             device = a[1]
-            self.retVal = ['DEVICE', device]
+            self.DEVICE = device
             return 
         if re.search('OVERWRITE*',line):
             a = line.split(' ');
             overwrite = a[1]
-            self.retVal = ['OVERWRITE', overwrite]
+            self.OVERWRITE = overwrite
             return 
         if re.search('\d{4}\/\d{1,2}\/\d{1,2}',line):
-            a = line.split(' ');
-            mondate1 = a[0]
-            mondate = mondate1[1:]
-            timehack1 = a[1]
-            timehack = timehack1[:-2]
-            data = a[2]
-            self.retVal = ['DATA', mondate, timehack, data]
+            #Taking advantage of standard format, not very spart parsing.
+            tempdate = line[1:17]
+            monitordate = tempdate.replace('/','-')
+            data = line[20:]
+            self.timeanddatum.append( [monitordate, data])
             return 
             
         err = 'Do not recognize: ' + line
--- telmon/test/test_parse.py
+++ telmon/test/test_parse.py
@@ -1,63 +1,77 @@
 import unittest
 import io
 
-from ..parseEDL import _EdlLine, parseStream
+from ..parseEDL import EdlFile, parseStream
 
 class TestExpMatch(unittest.TestCase):
 
-    def setup(self):
-        self.test_sourceline = '//SOURCE nwiswa EDL'
-        self.test_stationline = '//STATION 12113390'
-        self.test_deviceline = '//DEVICE GRFILTF-1'
-        self.test_overwriteline = '//OVERWRITE true'
-        self.test_dataline = '"2018/02/19 02:00", 3188.3097222222264'
-        self.test_bogusline = 'blahblah blah'
+    def test_initializeparseobject(self):
+         edl = EdlFile('Afile', '2017-12-12 10:00:00')
+         self.assertEqual(edl.filename, 'Afile')
+         self.assertEqual(edl.filedtg, '2017-12-12 10:00:00')
+    
+    def test_nofilename(self):
+        edl = EdlFile('', '2017-12-12 10:00:00')
+        self.assertTrue(edl.filename.startswith('assigned'))
+    
+    def test_baddateformat(self):
+        with self.assertRaises(ValueError):  
+            edl = EdlFile('Afile','2017/12/12 10:00:00')
+            
+    def test_nodate(self):
+        edl = EdlFile('Afile', '')
+        self.assertIsNotNone(edl.filedtg)
     
     def test_sourceline(self):
 
-        edlregex = _EdlLine('//SOURCE nwiswa EDL')
-        self.assertEqual(edlregex.retVal[0],'SOURCE')
-        self.assertEqual(edlregex.retVal[1], 'nwiswa')
+        edlFile = EdlFile('Afile', '2017-12-12 10:00:00')
+        edlFile.parseline('//SOURCE nwiswa EDL')
+        self.assertEqual(edlFile.SOURCE,'nwiswa')
         
     def test_stationline(self):
 
-        edlregex = _EdlLine('//STATION 12113390')
-        self.assertEqual(edlregex.retVal[0],'STATION')
-        self.assertEqual(edlregex.retVal[1], '12113390')
-        
+        edlFile = EdlFile('Afile', '2017-12-12 10:00:00')
+        edlFile.parseline('//STATION 12113390')
+        self.assertEqual(edlFile.STATION,'12113390')
+       
     def test_deviceline(self):
 
-        edlregex = _EdlLine('//DEVICE GRFILTF-1')
-        self.assertEqual(edlregex.retVal[0],'DEVICE')
-        self.assertEqual(edlregex.retVal[1], 'GRFILTF-1')
-        
+        edlFile = EdlFile('Afile', '2017-12-12 10:00:00')
+        edlFile.parseline('//DEVICE GRFILTF-1')
+        self.assertEqual(edlFile.DEVICE,'GRFILTF-1')
+       
     def test_overwriteline(self):
 
-        edlregex = _EdlLine('//OVERWRITE true')
-        self.assertEqual(edlregex.retVal[0],'OVERWRITE')
-        self.assertEqual(edlregex.retVal[1], 'true')
-        
+        edlFile = EdlFile('Afile', '2017-12-12 10:00:00')
+        edlFile.parseline('//OVERWRITE true')
+        self.assertEqual(edlFile.OVERWRITE,'true')
+         
     def test_dataline(self):
 
-        edlregex = _EdlLine('"2018/02/19 02:00", 3188.3097222222264')
-        self.assertEqual(edlregex.retVal[0],'DATA')
-        self.assertEqual(edlregex.retVal[1], '2018/02/19') 
-        self.assertEqual(edlregex.retVal[2], '02:00') 
-        self.assertEqual(edlregex.retVal[3], '3188.3097222222264') 
-        
-    def test_bogusline(self):
+        edlFile = EdlFile('Afile', '2017-12-12 10:00:00')
+        edlFile.parseline('"2018/02/19 02:00", 3188.3097222222264')
+        self.assertEqual(edlFile.timeanddatum[0][0],'2018-02-19 02:00')
+        self.assertEqual(edlFile.timeanddatum[0][1], '3188.3097222222264') 
 
+    def test_bogusline(self):
+ 
         with self.assertRaises(ValueError):  
-            edlregex = _EdlLine('blahblah blah')       
-    
+            edlFile = EdlFile('Afile', '2017-12-12 10:00:00')
+            edlFile.parseline('blahblah blah')       
+  
     def test_blankline(self):
-         with self.assertRaises(ValueError):  
-            edlregex = _EdlLine('')    
+    
+         with self.assertRaises(ValueError): 
+            edlFile = EdlFile('Afile', '2017-12-12 10:00:00')         
+            edlFile.parseline('blahblah blah')     
 
     def test_nothing(self):
-         with self.assertRaises(RuntimeError):  
-            edlregex = _EdlLine(None)              
+    
+         with self.assertRaises(RuntimeError): 
+            edlFile = EdlFile('Afile', '2017-12-12 10:00:00')         
+            edlFile.parseline(None)              
 
+"""
 class TestStreamInput(unittest.TestCase):  
 
     def test_goodstream(self):
@@ -69,7 +83,7 @@ class TestStreamInput(unittest.TestCase):
         input.write('"2018/02/19 02:00", 3188.3097222222264\n')
         retVal = parseStream(input)
         print(retVal)
-        
+"""        
             
 if __name__ == '__main__':
     unittest.main()
